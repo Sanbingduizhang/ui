c6ab6f773f0bd0ae96473428613b47c4a67ac887
冯鑫龙 <18862005869@163.com>
2018-03-16 16:31:53 +0800

    大更新页面，可以搜索等功能

---
+++ static/home/article/Method.js
@@ -0,0 +1,60 @@
+//内容左边的标签生成方法
+function su(data){
+    var article = data.data.data;
+    var cate_name = data.data.cate_name;
+    var len = article.length;
+    // 标签生成
+    var str = '';
+    for (var i = 0; i < len; i++) {
+    str +=  '<div class="con-left-con">'+
+                '<a href="javascript:void(0);" class="con-left-con-a">'+
+                    '<div class="con-left-top">'+
+                        '<span class="con-left-top-title">'+article[i].title.substring(0,20)+'...</span>'+
+                        '<span class="con-left-top-author">'+article[i].user_info.name+'&nbsp;&nbsp;&nbsp;&nbsp;'+article[i].updated_at+'</span>'+
+                    '</div>'+
+                    '<div class="con-left-mid">'+
+                        '<p class="con-left-mid-desc">'+article[i].content.substring(0,200)+'...</p>'+
+                    '</div>'+
+                    '<div class="con-left-bot">'+
+                        '<span class="con-left-bot-cate">分类:'+cate_name+'</span>'+
+                        '<span class="con-left-bot-like">点赞('+article[i].like+')&nbsp;&nbsp;评论('+article[i].comment_count+')</span>'+
+                    '</div>'+
+                '</a>'+
+            '</div>';
+        };
+        return str;
+};
+//推荐
+function rec(data){
+    var rec = data.data.articleRec;
+    var rec_len = rec.length;
+    str = '';
+    for(var i = 0;i < rec_len;i++){
+        str += '<li class="con-right-recon-li"><a href="javascript:void(0);" class="con-right-recon-a" temid = "'+rec[i].id+'">'+rec[i].title.substring(0,12)+'...</a></li>';
+    }
+    return str;
+}
+//热门
+function re(data){
+    var re = data.data.articleRe;
+    var re_len = re.length;
+    str = '';
+    for(var i = 0;i < re_len;i++){
+        str += '<li class="con-right-recon-li"><a href="javascript:void(0);" class="con-right-recon-a" mid = "'+re[i].id+'">'+re[i].title.substring(0,12)+'...</a></li>';
+    }
+    return str;
+}
+//分类
+function cates(data){
+    var cates = data.data.cates;
+    var cates_len = cates.length;
+    str = '';
+    for(var i = 0;i < cates_len;i++){
+        str += '<li class="con-right-li-li  con-right-cate"><a href="javascript:void(0);" class="con-right-a" catemid = "'+cates[i].id+'">'+cates[i].name+'</a></li>';
+    }
+    return str;
+}
+
+function load(){
+    
+}
--- static/home/article/article.js
+++ static/home/article/article.js
@@ -1,85 +1,44 @@
-function title(data){
-    var cate = data.data.cate;
-    var cate_len = cate.length; 
-    var str = '';
-    for(var i = 0;i < cate_len;i++) {
-        str += '<li><a href="javascript:void(0);">'+cate[i].name+'</a></li>';
-    }
-    return str;
-};
-//内容左边的标签生成方法
-function su(data){
-    var article = data.data.data;
-    var cate_name = data.data.cate_name;
-    var len = article.length;
-    // 标签生成
-    var str = '';
-    for (var i = 0; i < len; i++) {
-    str +=  '<div class="con-left-con">'+
-                '<a href="javascript:void(0);" class="con-left-con-a">'+
-                    '<div class="con-left-top">'+
-                        '<span class="con-left-top-title">'+article[i].title.substring(0,20)+'...</span>'+
-                        '<span class="con-left-top-author">'+article[i].user_info.name+'&nbsp;&nbsp;&nbsp;&nbsp;'+article[i].updated_at+'</span>'+
-                    '</div>'+
-                    '<div class="con-left-mid">'+
-                        '<p class="con-left-mid-desc">'+article[i].content.substring(0,200)+'...</p>'+
-                    '</div>'+
-                    '<div class="con-left-bot">'+
-                        '<span class="con-left-bot-cate">分类:'+cate_name+'</span>'+
-                        '<span class="con-left-bot-like">点赞('+article[i].like+')&nbsp;&nbsp;评论('+article[i].comment_count+')</span>'+
-                    '</div>'+
-                '</a>'+
-            '</div>';
-        };
-        return str;
-};
+
+//其他
 //内容右边的标签生成
-function re(data){
-    var cate = data.data.cate;
-    var cate_len = cate.length;
-    var str = '';
-    var str1 = '';
-    var str2 = '';
-    var str3 = '';
-    for (var i = 0; i < cate_len; i++) {
-        str1 =  '<div class="con-right-div">'+
-                    '<p><a href="javascript:void(0);">'+cate[i].name+'>>></a></p>'+
-                    '<div class="chayu-div">'+
-                        '<ul>';
-        for(var j = 0;j<cate[i].article.length;j++){
-            str2 += '<li><a href="javascript:void(0);">'+cate[i].article[j].title.substring(0,15)+'...</a></li>';
-        }
-        str3 = '</ul></div></div>';
-        str += str1 + str2 + str3;
-        str1 = str2 = str3 = '';
-        };
-    return str;
-};
 $(function (){
+    var id = Number(window.location.href.split('?')[1].split('=')[1]);
     $.ajax({
             type:"GET",
-            url:"http://www.heijiang.top/home/article/index/3",
+            url:"http://www.heijiang.top/home/article/index/"+id+"?right=769",
             dataType:"json",
             success:function(data){
+                //页码
                 var yema = data.data;
-                // var ul = $('<ul></ul>');
-                // var li = title(data);
-                // $('.header-left').html(ul.html(li));
+                //左边对应分类文章显示
                 var str = su(data);
-                // var ree = re(data);
                 $('.con-left').html(str);
-                // $('.con-right').html(ree);
+                //右边推荐文章
+                var recc = rec(data);
+                $('.con-right-recon-ul').html(recc);
+                //右边热门文正
+                var ree = re(data);
+                $('.con-right-re-ul').html(ree);
+                //右边分类
+                var catess = cates(data);
+                $('.con-right-li-ul').html(catess);
+                //分类标题
+                $('.cate').text(data.data.cate_name);
                 $('.M-box1').pagination({
+                    //页码总条数
                     totalData: yema.total,
+                    //页码每一页显示条数
                     showData: yema.per_page,
+                    //页码当前页
                     current:yema.current_page,
                     coping: true,
                     callback:function(api){
                         $.ajax({
                             type:"GET",
-                            url:"http://www.heijiang.top/home/article/index/3?page="+api.getCurrent(),
+                            url:"http://www.heijiang.top/home/article/index/"+id+"?page="+api.getCurrent(),
                             dataType:"json",
                             success:function(data){
+                                //左边对应分类的文章
                                 var str = su(data);
                                 $('.con-left').html(str);
                                 },
@@ -95,7 +54,6 @@ $(function (){
                 console.log(jqXHR);
             }
         });
-
     //登录功能
     $('#linkSub').click(function() {
         $('#login').show();
@@ -105,5 +63,102 @@ $(function (){
         $('#login').hide();
         $('#bg').hide();
     });
-    //小火箭向上滑动
+    //点击分类加载对应页面
+    $('.con-right-li-ul').on('click','.con-right-a',function(){
+        var catemid = $(this).attr('catemid');
+        $.ajax({
+            type:"GET",
+            url:"http://laravelgo.com/home/article/index/"+catemid,
+            dataType:"json",
+            success:function(data){
+                //页码
+                var yema = data.data;
+                //左边对应分类文章显示
+                var str = su(data);
+                $('.con-left').html(str);
+                //分类标题
+                $('.cate').text(data.data.cate_name);
+                $('.M-box1').pagination({
+                    //页码总条数
+                    totalData: yema.total,
+                    //页码每一页显示条数
+                    showData: yema.per_page,
+                    //页码当前页
+                    current:yema.current_page,
+                    coping: true,
+                    callback:function(api){
+                        $.ajax({
+                            type:"GET",
+                            url:"http://laravelgo.com/home/article/index/"+catemid+"?page="+api.getCurrent(),
+                            dataType:"json",
+                            success:function(data){
+                                //左边对应分类的文章
+                                var str = su(data);
+                                $('.con-left').html(str);
+                                },
+                            error:function (jqXHR){
+                                console.log(jqXHR);
+                            }
+            
+                        });
+                    },
+                }); 
+            },
+            error:function (jqXHR){
+                console.log(jqXHR);
+            }
+        });
+    });
+    //点击搜索，搜索对应页面
+    $('#btnSea').click(function(){
+        var serRes = $('#search').val();
+        if(!serRes){
+            alert('请输入搜索内容');
+        }else{
+            $.ajax({
+                type:"GET",
+                url:"http://www.heijiang.top/home/article/index?search="+serRes,
+                dataType:"json",
+                success:function(data){
+                    //页码
+                    var yema = data.data;
+                    //左边对应分类文章显示
+                    var str = su(data);
+                    $('.con-left').html(str);
+                    //分类标题
+                    $('.cate').text(data.data.cate_name);
+                    $('.M-box1').pagination({
+                        //页码总条数
+                        totalData: yema.total,
+                        //页码每一页显示条数
+                        showData: yema.per_page,
+                        //页码当前页
+                        current:yema.current_page,
+                        coping: true,
+                        callback:function(api){
+                            $.ajax({
+                                type:"GET",
+                                url:"http://www.heijiang.top/home/article/index?page="+api.getCurrent()+"&search="+serRes,
+                                dataType:"json",
+                                success:function(data){
+                                    //左边对应分类的文章
+                                    var str = su(data);
+                                    $('.con-left').html(str);
+                                    },
+                                error:function (jqXHR){
+                                    console.log(jqXHR);
+                                }
+                
+                            });
+                        },
+                    }); 
+                },
+                error:function (jqXHR){
+                    console.log(jqXHR);
+                }
+            });
+        };
+
+    });
 });
+
